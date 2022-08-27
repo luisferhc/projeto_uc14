0c6d1d924eec14bc7a3965d50bdb1ac24bd03053
unknown <heringcoelho@gmail.com>
2022-08-27 14:37:30 -0300

    uc14_er02

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using projeto_uc14.Models;
 using projeto_uc14.Repositories;
 
 namespace projeto_uc14.Controllers
@@ -21,6 +22,68 @@ namespace projeto_uc14.Controllers
         {
             try {
                 return Ok(_livroRepository.Listar());
+
+            } catch (Exception e) {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try {
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (livroBuscado == null) {
+
+                    return NotFound();
+                }
+
+                return Ok(livroBuscado);
+
+            } catch (Exception e) {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro l)
+        {
+            try {
+                _livroRepository.Cadastrar(l);
+
+                return StatusCode(201);
+
+            } catch (Exception e) {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro removido com sucesso");
+
+            } catch (Exception e) {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Livro l)
+        {
+            try {
+
+                _livroRepository.Alterar(id, l);
+
+                return StatusCode(204);
 
             } catch (Exception e) {
 
